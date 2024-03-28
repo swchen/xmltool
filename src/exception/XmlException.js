5a66298567528a230e26312ab77be63eb80470cc
chensw <swchenforgetful@hotmail.com>
2024-03-28 11:13:05 +0800

    feat: remove Error.captureStackTrace

--- src/exception/XmlException.js
+++ src/exception/XmlException.js
@@ -1,10 +1,7 @@
 export default class XmlException extends Error {
     constructor(message, line, column) {
         super(`${message} Line ${line}, column ${column}.`);
-        if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, XmlException);
-        }
-
+        
         this.line = line;
         this.column = column;
     }
